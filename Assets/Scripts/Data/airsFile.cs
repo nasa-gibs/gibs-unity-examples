015f34a53c0449c8641c95cdabf1ec505f0ecd3c
Angel Lim <brian.q.lim@gmail.com>
2020-06-24 13:51:43 -0700

    Created airsFile and airsParser files

---
+++ Assets/Scripts/Data/airsFile.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class airsFile {
+    public string path;
+    public string fileName;
+
+    public float x;
+    public float y;
+    public float z;
+    public float val;
+}
---
+++ Assets/Scripts/Data/airsParser.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using Debug = UnityEngine.Debug;
+
+public class airsParser : MonoBehaviour
+{
+    public static airsFile MetadataFromPath(string path)
+    {
+        airsFile dataFile = new airsFile();
+
+        dataFile.fileName = Path.GetFileName(path);
+        dataFile.path = path;
+
+        string[] plyOutput = File.ReadAllLines(path);
+        // Get vertex count
+        foreach (string currentLine in plyOutput)
+        {
+            string[] items = currentLine.Split(null, 3);
+            if (items != null && items.Length > 0)
+            {
+                switch (items[0])
+                {
+                    case "comment":
+                        if (items[1] == "x_label:")
+                            dataFile.x = items[2];
+                        if (items[1] == "y_label:")
+                            dataFile.y = items[2];
+                        if (items[1] == "z_label:")
+                            dataFile.z = items[2];
+                        if (items[1] == "val_label:")
+                            dataFile.val = items[2];
+                        if (items[1] == "min_val:")
+                        break;
+                    case "element":
+                        if (items[1] == "vertex")
+                            dataFile.vertexCount = int.Parse(items[2]);
+                        if (items[1] == "face")
+                            dataFile.triangleCount = int.Parse(items[2]);
+                        break;
+                }
+            }
+
+
+            if (items[0] == "end_header")
+                break;
+        }
+        dataFile.type = airsFile.DataType.pointcloud;
+        if (dataFile.identifier == null || dataFile.identifier.Trim() == "")
+            dataFile.identifier = Path.GetFileName(path);
+        Debug.Log(dataFile.identifier);
+        return dataFile;
+    }
+
+    public static airsFile ReadModelFromPath(string path, airsFile dataFile, DataObject dataObject, float loadWeight)
+    {
+        airsFile data = new airsFile();
+        string[] plyOutput = File.ReadAllLines(path);
+        if (dataFile == null)
+            dataFile = MetadataFromPath(path);
+
+        //Assign from DataFile
+        string variable = dataFile.identifier.Substring(dataFile.identifier.IndexOf('_') + 1);
+
+        data.fileName = dataFile.fileName;
+
+        data.x = dataFile.x;
+        data.y = dataFile.y;
+        data.z = dataFile.z;
+        data.val = dataFile.val;
+
+        int line = 0;
+        bool headerFlag = false;
+        List<float> x = new List<float>();
+        List<float> y = new List<float>();
+        List<float> z = new List<float>();
+        List<float> val = new List<float>();
+        foreach (string currentLine in plyOutput)
+        {
+            string[] items = currentLine.Split(' ');
+
+            // Skip header
+            if (!headerFlag)
+            {
+                if (items[0] == "end_header")
+                {
+                    headerFlag = true;
+                }
+                continue;
+            }
+
+            x.Add(float.Parse(items[0]));
+            y.Add(float.Parse(items[1]));
+            z.Add(float.Parse(items[2]));
+            val.Add(float.Parse(items[3]));
+
+            if (line % 1000 == 0 || line == plyOutput.Length - 1)
+            {
+                ThreadManager.instance.callbacks.Add(() => {
+                    dataObject.UpdateLoadPercent((float)line / plyOutput.Length * loadWeight, "Parsing File");
+                });
+            }
+            line++;
+
+        }
+
+        data.xIndexDefault = "mesh.x";
+        data.yIndexDefault = "mesh.y";
+        data.zIndexDefault = "mesh.z";
+        data.colorIndexDefault = variable;
+
+        data.vars[data.xIndexDefault] = x.ToArray();
+        data.vars[data.yIndexDefault] = y.ToArray();
+        data.vars[data.zIndexDefault] = z.ToArray();
+        data.vars[data.colorIndexDefault] = val.ToArray();
+
+        return data;
+    }
+
+    private static string ToTitleCase(string str)
+    {
+        if (str != "")
+            return char.ToUpper(str[0]) + ((str.Length > 1) ? str.Substring(1) : string.Empty);
+        return string.Empty;
+    }
+
+}
