c97db15a0c734acdf38a9bfa8f59aa81ca143f31
Angel Lim <brian.q.lim@gmail.com>
2020-06-25 11:51:46 -0700

    Added CSV parser to read AIRS data

--- Assets/Scripts/Data/airsParser.cs
+++ Assets/Scripts/Data/airsParser.cs
@@ -13,9 +13,9 @@ public class airsParser
         dataFile.fileName = Path.GetFileName(path);
         dataFile.path = path;
 
-        string[] plyOutput = File.ReadAllLines(path);
-        // Get vertex count
-        foreach (string currentLine in plyOutput)
+        string[] airsOutput = File.ReadAllLines(path);
+        // Get AIRS values
+        foreach (string currentLine in airsOutput)
         {
             string[] items = currentLine.Split(null, 3);
             if (items != null && items.Length > 0)
@@ -34,88 +34,9 @@ public class airsParser
                         break;
                 }
             }
-
-
             if (items[0] == "end_header")
                 break;
         }
-        dataFile.type = airsFile.DataType.pointcloud;
-        if (dataFile.identifier == null || dataFile.identifier.Trim() == "")
-            dataFile.identifier = Path.GetFileName(path);
-        Debug.Log(dataFile.identifier);
         return dataFile;
     }
-
-    public static airsFile ReadModelFromPath(string path, airsFile dataFile, DataObject dataObject, float loadWeight)
-    {
-        airsFile data = new airsFile();
-        string[] plyOutput = File.ReadAllLines(path);
-        if (dataFile == null)
-            dataFile = MetadataFromPath(path);
-
-        //Assign from DataFile
-        string variable = dataFile.identifier.Substring(dataFile.identifier.IndexOf('_') + 1);
-
-        data.fileName = dataFile.fileName;
-
-        data.x = dataFile.x;
-        data.y = dataFile.y;
-        data.z = dataFile.z;
-        data.val = dataFile.val;
-
-        int line = 0;
-        bool headerFlag = false;
-        List<float> x = new List<float>();
-        List<float> y = new List<float>();
-        List<float> z = new List<float>();
-        List<float> val = new List<float>();
-        foreach (string currentLine in plyOutput)
-        {
-            string[] items = currentLine.Split(' ');
-
-            // Skip header
-            if (!headerFlag)
-            {
-                if (items[0] == "end_header")
-                {
-                    headerFlag = true;
-                }
-                continue;
-            }
-
-            x.Add(float.Parse(items[0]));
-            y.Add(float.Parse(items[1]));
-            z.Add(float.Parse(items[2]));
-            val.Add(float.Parse(items[3]));
-
-            if (line % 1000 == 0 || line == plyOutput.Length - 1)
-            {
-                ThreadManager.instance.callbacks.Add(() => {
-                    dataObject.UpdateLoadPercent((float)line / plyOutput.Length * loadWeight, "Parsing File");
-                });
-            }
-            line++;
-
-        }
-
-        data.xIndexDefault = "mesh.x";
-        data.yIndexDefault = "mesh.y";
-        data.zIndexDefault = "mesh.z";
-        data.colorIndexDefault = variable;
-
-        data.vars[data.xIndexDefault] = x.ToArray();
-        data.vars[data.yIndexDefault] = y.ToArray();
-        data.vars[data.zIndexDefault] = z.ToArray();
-        data.vars[data.colorIndexDefault] = val.ToArray();
-
-        return data;
-    }
-
-    private static string ToTitleCase(string str)
-    {
-        if (str != "")
-            return char.ToUpper(str[0]) + ((str.Length > 1) ? str.Substring(1) : string.Empty);
-        return string.Empty;
-    }
-
 }
