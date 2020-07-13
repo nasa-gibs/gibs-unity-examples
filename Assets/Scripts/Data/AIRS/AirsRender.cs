5408018173999248cde2c4586465d04789d85421
Angel Lim <brian.q.lim@gmail.com>
2020-07-13 07:01:06 -0700

    Adjustments to AIRS Visualizer

--- Assets/Scripts/Data/AIRS/AirsRender.cs
+++ Assets/Scripts/Data/AIRS/AirsRender.cs
@@ -6,7 +6,9 @@ using UnityEngine;
 
 public class AirsRender : MonoBehaviour
 {
+    public GameObject gibsObject;
     public GameObject[] dataPoint;
+
 
     // Start is called before the first frame update
     void Start()
@@ -19,15 +21,15 @@ public class AirsRender : MonoBehaviour
         for (int i = 0; i < dataLength; i++)
         {
             dataPoint[i] = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            dataPoint[i].transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
             var airsRenderer = dataPoint[i].GetComponent<Renderer>();
             float colorValue = airsData.val[i];
             
             float sphereRadius = GetComponent<SphereCollider>().radius;
             float actualRadius = 3948.8F;
-            float elevationScale = 100F;
+            float elevationScale = .70F;
 
            //WIP Datapoint Color System
-           
             if(colorValue > 0.0F && colorValue < 10.0F)
                 airsRenderer.material.SetColor("_Color", Color.red);
             if(colorValue > 10.0F && colorValue < 20.0F)
@@ -48,13 +50,13 @@ public class AirsRender : MonoBehaviour
                 airsRenderer.material.SetColor("_Color", Color.grey);
             if (colorValue > 90.0F && colorValue < 100.0F)
                 airsRenderer.material.SetColor("_Color", Color.white);
-                
-
 
             // public static Vector3 LatLongToXYZ(float latitude, float longitude, float elevation,
             //                      float actualRadius, float sphereRadius, float elevationScale)
             Vector3 airsCords = Coordinates.LatLongToXYZ(airsData.x[i], airsData.y[i], airsData.z[i], actualRadius, sphereRadius, elevationScale);
-            Instantiate(dataPoint[i], airsCords, Quaternion.identity);
+            Instantiate(dataPoint[i], transform.position + airsCords, Quaternion.identity);
+            //dataPoint[i].transform.parent = gibsObject.transform;
         }
     }
+
 }
