1fd6945c7014aa9ec5237897bc21fa5a989db6f3
Angel Lim <brian.q.lim@gmail.com>
2020-07-17 11:17:28 -0700

    Casted Resources.Load() as <Texture>

--- Assets/Scripts/Data/Calipso/CalipsoInstantiator.cs
+++ Assets/Scripts/Data/Calipso/CalipsoInstantiator.cs
@@ -15,7 +15,7 @@ public class CalipsoInstantiator : MonoBehaviour
     {
         for (int i = 0; i < calipsoData.features.Length; i++)
         {
-            profiles.LoadImage(calipsoData.features[i].properties.img);
+            profiles = Resources.Load <Texture> (calipsoData.features[i].properties.img);
             curtain.Generate(calipsoData.features[i].properties.y_range[1], calipsoData.features[i].properties.x_range[1], calipsoData.features[i].properties.y_range[2], calipsoData.features[i].properties.x_range[2], profiles);
         }
     }
